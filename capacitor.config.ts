9f3bad9496836c696febcbb6433bf4c9068128c3
Luis Meza <luimi@live.com>
2025-05-12 14:41:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bingoPlayer',
+  webDir: 'dist'
+};
+
+export default config;
