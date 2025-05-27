25d5c658caeb3a3c4eeb79b3cfb64c8e31f6cf11
Luis Meza <luimi@live.com>
2025-05-27 14:22:45 -0500

    ajuste appid

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'bingoPlayer',
+  appId: 'com.lui2mi.bingoplayer',
+  appName: 'Bingo Player',
   webDir: 'dist'
 };
 
