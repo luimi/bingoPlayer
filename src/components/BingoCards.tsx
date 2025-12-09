5f00c36481e1402d291c1fa84eff8a3d18b2387e
Luis Meza <luimi@live.com>
2025-12-09 11:02:44 -0500

    agregado validacion de servidor

--- src/components/BingoCards.tsx
+++ src/components/BingoCards.tsx
@@ -1,7 +1,7 @@
 import { IonCol, IonGrid, IonIcon, IonRow, IonSpinner, useIonAlert, useIonRouter, useIonToast } from "@ionic/react";
 import { useBingoContext } from "../contexts/BingoContext";
 import { Link } from "react-router-dom";
-import { add, addCircleOutline, camera } from "ionicons/icons";
+import { add, addCircleOutline, camera, informationCircle, warningOutline } from "ionicons/icons";
 import { useRef, useState } from "react";
 import { setCard, setCards } from "../utils/BingoController";
 import MiniBingoCard from "./MiniBingoCard";
@@ -10,7 +10,7 @@ import '../utils/I18n';
 import { useTranslation } from "react-i18next";
 import { gaEvent } from "../utils/analytics";
 import Scan from "./Scan";
-import { getScan } from "../utils/serverController";
+import { getScan, getStatus } from "../utils/serverController";
 import CropModal from "./Crop";
 import { InterstitialAd } from "@capgo/capacitor-admob";
 import { Capacitor } from "@capacitor/core";
@@ -37,9 +37,10 @@ const BingoCards: React.FC<ComponentProps> = () => {
         } else {
             present({
                 message: t("bingoCards.error.cardsWithAI"),
-                duration: 3000,
+                duration: 5000,
                 position: 'top',
                 color: 'danger',
+                icon: warningOutline
             })
         }
     }
@@ -52,6 +53,7 @@ const BingoCards: React.FC<ComponentProps> = () => {
         await interstitial.load();
         await interstitial.show();
     }
+
 
     return (
         <IonGrid>
@@ -71,8 +73,28 @@ const BingoCards: React.FC<ComponentProps> = () => {
                         },
                         {
                             icon: <>{loading ? <IonSpinner></IonSpinner> : <IonIcon icon={camera} />}</>,
-                            action: () => {
-                                scanRef.current?.click()
+                            action: async () => {
+                                try {
+                                    const result = await getStatus()
+                                    if (result.status) scanRef.current?.click()
+                                    else present({
+                                        message: t("bingoCards.error.unavailable"),
+                                        duration: 5000,
+                                        position: 'top',
+                                        color: 'warning',
+                                        icon: informationCircle
+                                    })
+                                } catch (e) {
+                                    present({
+                                        message: t("bingoCards.error.server"),
+                                        duration: 5000,
+                                        position: 'top',
+                                        color: 'warning',
+                                        icon: informationCircle
+                                    })
+                                }
+
+
                             }
                         }
                     ]} />
